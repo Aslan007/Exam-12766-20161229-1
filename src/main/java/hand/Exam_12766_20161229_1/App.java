e923ddbfb060a628574cb07283c80c898fdd8826
Aslan007 <912447193@qq.com>
2016-12-29 18:07:46 +0800

    新建项目，未过滤

---
+++ src/main/java/hand/Exam_12766_20161229_1/App.java
@@ -0,0 +1,13 @@
+package hand.Exam_12766_20161229_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
